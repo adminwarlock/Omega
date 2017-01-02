3f19ae157cdb58f1b4884aded470c4dfdd874fb6
adminwarlock <cwowctm@gmail.com>
2017-01-02 14:10:54 +0800

    contents

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="goods">
-    <div class="menu-wrapper">
+    <div class="menu-wrapper" v-el:menu-wrapper>
       <ul>
-        <li v-for="item in goods" class="menu-item">
+        <li v-for="item in goods" class="menu-item" v-bind:class="{'current':currentIndex === $index}" @click="selectMenu($index,$event)">
           <span class="text">
             <span v-show="item.type > 0" class="icon" v-bind:class="classMap[item.type]"></span>{{item.name}}
           </span>
         </li>
       </ul>
     </div>
-    <div class="foods-wrapper">
+    <div class="foods-wrapper" v-el:foods-wrapper>
       <ul>
-        <li v-for="item in goods" class="food-list">
+        <li v-for="item in goods" class="food-list food-list-hook">
           <h1 class="title">{{item.name}}</h1>
           <ul>
             <li v-for="food in item.foods" class="food-item">
@@ -22,12 +22,10 @@
                 <h2 class="name">{{food.name}}</h2>
                 <p class="desc">{{food.description}}</p>
                 <div class="extra">
-                  <span>月售{{food.sellCount}}</span>
-                  <span>好评率{{food.rating}}%</span>
+                  <span class="count">月售{{food.sellCount}}</span><span>好评率{{food.rating}}%</span>
                 </div>
                 <div class="price">
-                  <span>￥{{food.price}}</span>
-                  <span v-show="food.oldPrice">￥{{food.oldPrice}}</span>
+                  <span class="now">￥{{food.price}}</span><span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
                 </div>
               </div>
             </li>
@@ -35,11 +33,13 @@
         </li>
       </ul>
     </div>
+    <shopcart></shopcart>
   </div>
-
 </template>
 
 <script type="text/ecmascript-6">
+    import BScroll from 'better-scroll';
+    import shopcart from 'components/shopcart/shopcart.vue';
     const ERR_OK = 0;
     export default {
         props: {
@@ -49,17 +49,71 @@
         },
         data() {
               return {
-                  goods: []
+                  goods: [],
+                  listHeight: [],
+                  scrollY: 0
               };
+        },
+        computed: {
+            currentIndex() {
+                for (let i = 0; i < this.listHeight.length; i++) {
+                    let height1 = this.listHeight[i];
+                    let height2 = this.listHeight[i + 1];
+                    if (!height2 || (this.scrollY >= height1 && this.scrollY < height2)) {
+                        return i;
+                    };
+                }
+                return 0;
+            }
         },
         created() {
             this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
             this.$http.get('/api/goods').then((response) => {
                 response = response.body;
                 if (response.errno === ERR_OK) {
-                  this.goods = response.data;
+                    this.goods = response.data;
+                    this.$nextTick(() => {
+                        //  滚动
+                        this._initScroll();
+                        //  计算高度
+                        this._calculateHeight();
+                    });
+                }
+            });
+        },
+        methods: {
+            selectMenu(index, event) {
+                if (!event._constructed) {
+                    return;
+                };
+                let foodList = this.$els.foodsWrapper.getElementsByClassName('food-list-hook');
+                let el = foodList[index];
+                this.foodsScroll.scrollToElement(el, 300);
+            },
+            _initScroll() {
+                this.menuScroll = new BScroll(this.$els.menuWrapper, {
+                    click: true
+                });
+                this.foodsScroll = new BScroll(this.$els.foodsWrapper, {
+                    probeType: 3
+                });
+                this.foodsScroll.on('scroll', (pos) => {
+                    this.scrollY = Math.abs(Math.round(pos.y));
+                });
+            },
+            _calculateHeight() {
+                let foodList = this.$els.foodsWrapper.getElementsByClassName('food-list-hook');
+                let height = 0;
+                this.listHeight.push(height);
+                for (let i = 0; i < foodList.length; i++) {
+                    let item = foodList[i];
+                    height += item.clientHeight;
+                    this.listHeight.push(height);
+                };
             }
-          });
+        },
+        components: {
+            shopcart
         }
     };
 </script>
@@ -84,6 +138,14 @@
         width :56px
         height : 54px
         line-height :14px
+        &.current
+          position:relative
+          z-index: 10
+          margin-top: -1px
+          background: #fff
+          font-weight:bold
+          .text
+            border:none
         .icon
           display:inline-block
           vertical-align:top
@@ -137,4 +199,28 @@
             line-height :14px
             font-size :14px
             color :rgb(7,17,27)
+          .desc,.extra
+            line-height:10px
+            font-size:10px
+            color:rgb(147,153,159)
+          .desc
+            margin-bottom:8px
+            line-height:12px
+          .extra
+            .count
+              margin-right:12px
+          .price
+            font-weight:bold
+            line-height:24px
+            .now
+              margin-right:8px
+              font-size:14px
+              color:rgb(240,20,20)
+            .old
+              text-decoration:line-through
+              font-size:10px
+              color:rgb(147,153,159)
+
+
+
 </style>
